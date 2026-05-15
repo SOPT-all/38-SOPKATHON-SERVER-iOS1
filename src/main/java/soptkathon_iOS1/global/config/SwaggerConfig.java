a096f6035988feb6f8ecdccad9f562b0458c06a9
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-05-16 01:13:41 +0900

    setting: 공통 설정 추가

---
+++ src/main/java/soptkathon_iOS1/global/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package soptkathon_iOS1.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Soptkathon iOS1 API")
+                        .description("솝커톤 iOS1조 API 문서")
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
