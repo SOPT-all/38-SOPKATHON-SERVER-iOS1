132c12938132e6890c0e8d343fb9a0e1845f415e
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-05-17 04:30:43 +0900

    feat: 목표 생성 성공 service 구현

--- src/main/java/soptkathon_iOS1/domain/goal/service/GoalService.java
+++ src/main/java/soptkathon_iOS1/domain/goal/service/GoalService.java
@@ -3,14 +3,20 @@ package soptkathon_iOS1.domain.goal.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import soptkathon_iOS1.domain.goal.dto.request.GoalCreateRequest;
+import soptkathon_iOS1.domain.goal.dto.response.GoalCreateResponse;
 import soptkathon_iOS1.domain.goal.dto.response.GoalListResponse;
 import soptkathon_iOS1.domain.goal.entity.Goal;
+import soptkathon_iOS1.domain.goal.entity.GoalStatus;
 import soptkathon_iOS1.domain.goal.repository.GoalRepository;
+import soptkathon_iOS1.domain.user.entity.User;
 import soptkathon_iOS1.domain.user.exception.UserErrorCode;
 import soptkathon_iOS1.domain.user.exception.UserException;
 import soptkathon_iOS1.domain.user.repository.UserRepository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
@@ -32,6 +38,26 @@ public class GoalService {
                 .toList();
 
         return response;
+    }
+
+    @Transactional
+    public GoalCreateResponse createGoal(Long userId, GoalCreateRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+
+        LocalDateTime expiredAt = LocalDate.parse(request.expiredAt(), DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+                .atTime(23, 59, 59);
+
+        Goal goal = Goal.builder()
+                .user(user)
+                .title(request.title())
+                .status(GoalStatus.IN_PROGRESS)
+                .expiredAt(expiredAt)
+                .build();
+
+        Goal savedGoal = goalRepository.save(goal);
+
+        return GoalCreateResponse.from(savedGoal);
     }
 
     private String calculateDDay(Goal goal) {
