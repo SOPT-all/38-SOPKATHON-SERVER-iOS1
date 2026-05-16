1fd495ac045d2091d0d4d9f493bcab59ca58af89
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-05-16 23:49:37 +0900

    refactor: https 반영

--- src/main/java/soptkathon_iOS1/global/config/SwaggerConfig.java
+++ src/main/java/soptkathon_iOS1/global/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package soptkathon_iOS1.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,6 +10,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/")
+        }
+)
 public class SwaggerConfig {
 
     @Bean
