8a6cd8d4f508e994cd0cbdb5af1da04d31889213
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-05-17 04:30:51 +0900

    feat: 목표 생성 성공 controller 구현

--- src/main/java/soptkathon_iOS1/domain/goal/controller/GoalController.java
+++ src/main/java/soptkathon_iOS1/domain/goal/controller/GoalController.java
@@ -1,11 +1,11 @@
 package soptkathon_iOS1.domain.goal.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import soptkathon_iOS1.domain.goal.dto.request.GoalCreateRequest;
+import soptkathon_iOS1.domain.goal.dto.response.GoalCreateResponse;
 import soptkathon_iOS1.domain.goal.dto.response.GoalListResponse;
 import soptkathon_iOS1.domain.goal.service.GoalService;
 import soptkathon_iOS1.global.common.dto.BaseResponse;
@@ -23,5 +23,15 @@ public class GoalController {
     public ResponseEntity<BaseResponse<List<GoalListResponse>>> getGoals(@PathVariable Long userId) {
         List<GoalListResponse> goals = goalService.getGoalsByUserId(userId);
         return ResponseEntity.ok(BaseResponse.success(SuccessCode.GET_GOAL_LIST_SUCCESS, goals));
+    }
+
+    @PostMapping("/{userId}/goals")
+    public ResponseEntity<BaseResponse<GoalCreateResponse>> createGoal(
+            @PathVariable Long userId,
+            @RequestBody GoalCreateRequest request
+    ) {
+        GoalCreateResponse response = goalService.createGoal(userId, request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(BaseResponse.success(SuccessCode.CREATE_GOAL_SUCCESS, response));
     }
 }
